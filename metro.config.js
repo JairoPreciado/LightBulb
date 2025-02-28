49e2e6b7317b7b7b0d752555f4a1ce078ff75ace
JairoPA <jairopreciadoayala@gmail.com>
2025-02-27 20:56:28 -0600

    actualizacion

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-
-module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
-  // Agrega 'bin' a la lista de extensiones de asset
-  config.resolver.assetExts.push('bin');
-  return config;
-})();
